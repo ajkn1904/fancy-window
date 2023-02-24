469e05ab25892cb39cc14a8dfce502b377098524
ajkn1904 <ajknishat@gmail.com>
2023-02-24 22:09:50 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
+import Header from './Components/Header/Header';
+import Gallery from './Components/Gallery/Gallery';
 
 function App() {
+
+  const [getImages, setGetImages] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_ACCESS_KEY}`)
+    .then(res => res.json())
+    .then(data => setGetImages(data))
+  }, []);
+
+
+
+
+//console.log(getImages)
+
   return (
-    <div className="App">
-      hello
+    <div className="App max-w-[1440]">
+      <NavBar/>
+      <Header/>
+      <Gallery/>
+
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const NavBar = () => {
+
+    const menu = <>
+        <li><a href='#explore' className='font-bold text-xs text-[#333333] pr-1'>Explore</a></li>
+        <li><a href='#collection' className='font-bold text-xs text-[#333333] pr-1'>Collection</a></li>
+        <li><a href='#community' className='font-bold text-xs text-[#333333]'>Community</a></li>
+    </>
+
+
+    return (
+        <div className="navbar h-[70px] lg:h-[97px]">
+            <div className="navbar-start">
+
+                <a href='#home' className="text-[24px] lg:text-3xl font-normal text-[#333333] ml-[27px] pt-[18px] pb-[19px] lg:pt-[27px] lg:mb-7">Image Gallery</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <div className="form-control">
+                        <input type="text" placeholder="Search images here" className="input input-bordered h-[43px] w-[419px] ml-16 mr-[27px]" />
+                    </div>
+                    {menu}
+                </ul>
+            </div>
+            <div className="hidden lg:flex ml-[88px] mr-[150px]">
+                <label className="label cursor-pointer">
+                    <span className="font-bold text-xs text-[#333333] w-[69px]">Dark Mode</span>
+                    <input type="checkbox" className="toggle" />
+                </label>
+
+            </div>
+
+
+            <button className="ml-[30%] mr-[13.05px] btn btn-ghost btn-circle lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+            </button>
+
+
+
+            <div className="dropdown dropdown-end mr-[31px]">
+                <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                </label>
+                <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content rounded-box w-40">
+
+
+                    {menu}
+                    <li>
+                        <label className="label cursor-pointer pr-1">
+                            <span className="font-bold text-xs text-[#333333]">Dark Mode</span>
+                            <input type="checkbox" className="toggle" />
+                        </label>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+    );
+};
+
+export default NavBar;
